216ada4d8f7893d87300c59fa9310d9164031ed3
shankar <saishankar.g@apcfss.in>
2026-04-23 15:33:32 +0530

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <LoginPage />
-  );
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/hello" element={<>HI</>} />
+      <Route path="/bye" element={<>BYE</>} />
+      <Route path="/sleep" element={<>SLEEP</>} />
+    </Routes>);
 }
 
 export default App;
